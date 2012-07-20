33dd66cfbdd44b97c89ef4441f16da9939bd6ad7
Erik Andrejko <erik@climate.com>
2012-07-19 22:38:32 -0700

    Update org-mode to not eval on export.

--- init.el
+++ init.el
@@ -169,7 +169,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(haskell-font-lock-symbols t)
- '(org-src-fontify-natively t))
+ '(org-src-fontify-natively t)
+ '(org-export-babel-evaluate nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
